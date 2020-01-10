2f958f109bfe4c9118db279dc88671c5216cd6f4
tavisca-akhatri <akhatri@tavisca.com>
2020-01-10 14:45:18 +0530

    schedule and delete api call done

--- src/JobsList/ShowForm.js
+++ src/JobsList/ShowForm.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import SchedulerForm from '../JobSchedulerForm/schedulerForm';
-import './JobsTable.css';
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+import SchedulerForm from "../JobSchedulerForm/schedulerForm";
+import "./JobsTable.css";
 
 function ShowForm(props) {
-
-    return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            scrollable
-        >
-            <Modal.Header className="modal-header" closeButton>
-                <h4>Fill in the required details for scheduling the job</h4>
-            </Modal.Header>
-            <Modal.Body>
-            <SchedulerForm jobName={props.name} setScheduledJobsData={props.setScheduledJobsData} />
-            </Modal.Body>
-            <Modal.Footer>
-                <Button className="button" onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    );
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      scrollable
+    >
+      <Modal.Header className="modal-header" closeButton>
+        <h4>Fill in the required details for scheduling the job</h4>
+      </Modal.Header>
+      <Modal.Body>
+        <SchedulerForm
+          jobName={props.name}
+          getScheduledJobs={props.getScheduledJobs}
+          setScheduledJobsData={props.setScheduledJobsData}
+          setCronExpression = {props.setCronExpression}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button className="button" onClick={props.onHide}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 export default ShowForm;
--- src/ScheduleJobList/ScheduleJobList.js
+++ src/ScheduleJobList/ScheduleJobList.js
@@ -4,47 +4,39 @@ import Start from '../Assets/start-icon.png';
 import Stop from '../Assets/stop-icon.png';
 import Delete from '../Assets/delete-icon.png';
 import './ScheduleJobList.css'
-import {deleteScheduledJob} from  '../Services/services'
+import {deleteScheduledJob} from '../Services/services'
 
 const headers = ["Job Name", "Country", "Job Schedule Time", "Job Last Fired Time",
     "Job Next Fired Time", "Action", "Status"];
 
 function ScheduleJobList(props) {
 
-    const [status, setStatus] = React.useState("Scheduled");
 
     const data = props.scheduledJobsData;
     
     const startJob = (index) => {
-        setStatus("Running")
+        props.startScheduledJob(index);
     }
-
-    const stopJob = (index) => {
-        props.stopScheduledJobs(index);
+    const stopJob = (index) =>{
+        props.stopScheduledJobs(index)
     }
-
-    const deleteJob = (index,scheduledJob) => {
-
-        let jsondata = {
-            jobName:scheduledJob.jobName,
-            country:scheduledJob.country,
-            cronExpression:scheduledJob.cronExpression
-        }
-        
-        deleteJobRequest(index,jsondata);
+    
+    const deleteJob = (jobKey) => {
+        console.log(jobKey);
+        deleteJobRequest(jobKey);
     }
-
-   const deleteJobRequest=async (index,jsondata)=>{
-        let response=await deleteScheduledJob(jsondata);
+ 
+    const deleteJobRequest=async (jobKey)=>{
+        let response=await deleteScheduledJob(jobKey);
         let {status}=response;
         console.log("deleted status res",status);
         if(status===200)
-            props.deleteScheduledJobs(index);
+            props.getScheduledJobs();
     }
-
 
     return (
         <div className="scheduletable">
+
 
             <Table bordered hover >
 
@@ -55,8 +47,10 @@ function ScheduleJobList(props) {
                         })}
                     </tr>
                 </thead>
+                
                 <tbody>
-                    {data.map((item, index) => {
+                    {
+                    data.map((item, index) => {
                         return (
                             <tr>
                                 <td>{item.jobName}</td>
@@ -65,13 +59,9 @@ function ScheduleJobList(props) {
                                 <td>{item.lastScheduledTime}</td>
                                 <td>{item.nextScheduledTime}</td>
                             
-
-                                <td><img className="actionIcons" src={Start} 
-                                        onClick={() => startJob(index)} alt="Not found" />
-                                    <img className="actionIcons" src={Stop} onClick={()=>stopJob(index)} alt="Not found" />{' '}{' '}{' '}{' '}
-                                    <img className="actionIcons" src={Delete} 
-                                        onClick={() => deleteJob(index,item)} alt="Not found" /></td>
-
+                                <td><img className="actionIcons" src={Start} onClick={() => startJob(index)} alt="Not found" />
+                                    <img className="actionIcons" src={Stop} alt="Not found"  onClick ={() => stopJob(index)}/>
+                                    <img className="actionIcons" src={Delete} onClick={() => deleteJob(item.jobKey)} alt="Not found" /></td>
                                 <td>{item.status}</td>
                             </tr>
                         );
